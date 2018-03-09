2fd6d0ea0d5394e4c3ab148b8b2149dcf8ebf985
ashleijo <shuryjo@gmail.com>
2018-03-09 12:03:58 -0500

    adding two server functions to get current and yesterdays values

--- src/app.js
+++ src/app.js
@@ -1,16 +1,35 @@
 const bodyParser = require('body-parser');
 const express = require('express');
-
+const fetch = require('node-fetch');
+const config = require('../config.js');
 const server = express();
-const STATUS_USER_ERROR = 422;
 const STATUS_SUCCESS = 200;
-const PORT = 3000;
-const config = require("../config.js");
+const STATUS_USER_ERROR = 422;
+const currentURL = 'https://api.coindesk.com/v1/bpi/currentprice/USD.json';
+const historicalURL = 'https://api.coindesk.com/v1/bpi/historical/close.json';
 
-server.listen(PORT, (err) => {
+server.use(bodyParser.json());
+
+server.use((req, res, next) => {
+  fetch(`${historicalURL}?for=yesterday`)
+  .then(res => res.json())
+  .then(json => console.log('yesterday -> ' + Object.values(json.bpi)[0]));
+  next();
+});
+
+
+
+server.get("/compare", (req, res) => {
+  fetch(`${currentURL}`)
+    .then(res => res.json())
+    .then(json => console.log('today -> ' + json.bpi.USD.rate));
+});
+
+server.listen(config.port, (err) => {
   if (err) {
-    console.log("errror \n" + err);
+    console.log("Error: \n" + err);
   } else {
-    console.log(`Server is listening on ${PORT}`);
+    console.log("... .... ... .. ... .... ... ..");
+    console.log(`...Server is listening on ${config.port}!`);
   }
 });
